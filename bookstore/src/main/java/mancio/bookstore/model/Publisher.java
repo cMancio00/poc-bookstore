417ab58ec91681ec29471d7a164cdb39fa608872
Christian Mancini <mancinichristian00@gmail.com>
2024-10-21 15:47:57 +0200

    Added Hibernate
    Created configuration and annotated Publisher class
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- bookstore/src/main/java/mancio/bookstore/model/Publisher.java
+++ bookstore/src/main/java/mancio/bookstore/model/Publisher.java
@@ -1,17 +1,29 @@
 package mancio.bookstore.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Publisher")
 public class Publisher {
-	private String id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	@Column(name = "name", nullable = false)
 	private String name;
 
 	public Publisher() {}
 	
-	public Publisher(String id, String name) {
-		this.id = id;
+	public Publisher(String name) {
 		this.name = name;
 	}
 
-	public String getId() {
+	public int getId() {
 		return id;
 	}
 
@@ -21,5 +33,10 @@ public class Publisher {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Publisher [id=" + id + ", name=" + name + "]";
 	}	
 }
