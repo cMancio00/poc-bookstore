1f5bac9a6284c27ad72fd7cd56aa02c7bc136c95
Christian Mancini <mancinichristian00@gmail.com>
2024-10-23 14:29:42 +0200

    Using in-memory database for unit Tests Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- bookstore/src/test/java/mancio/bookstore/repository/PublisherRepositoryHibernateTest.java
+++ bookstore/src/test/java/mancio/bookstore/repository/PublisherRepositoryHibernateTest.java
@@ -4,89 +4,66 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import mancio.bookstore.model.Publisher;
 
 class PublisherRepositoryHibernateTest {
-	private static final Logger LOGGER = LogManager.getLogger(PublisherRepositoryHibernateTest.class);
-	private static final String MYSQL_DATABASE = "bookstore";
-	private static final int MYSQL_PORT = 3306;
-	private static final  String CONNECTION_URL = String.format("jdbc:mysql://localhost:%d/%s", MYSQL_PORT, MYSQL_DATABASE);
-	
-	
+	private static final String H2_DATABASE = "bookstore";
+	private static final String CONNECTION_URL = String.format("jdbc:h2:mem:%s", H2_DATABASE);
+
 	private static SessionFactory sessionFactory;
 	private PublisherRepositoryHibernate publisherRepository;
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		System.setProperty("hibernate.connection.url",CONNECTION_URL);
-		System.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-		System.setProperty("hibernate.connection.username", "bookstore-manager");
-		System.setProperty("hibernate.connection.password", "test");
-		LOGGER.info("Connecting to {}", CONNECTION_URL);
-		sessionFactory = new Configuration().configure().buildSessionFactory();
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() {
-		sessionFactory.close();
-	}
 
 	@BeforeEach
 	void setUp() {
-		sessionFactory.getCache().evictAllRegions();
+		System.setProperty("hibernate.connection.url", CONNECTION_URL);
+		System.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+		System.setProperty("hbm2ddl.auto", "create-drop");
+		sessionFactory = new Configuration().configure().buildSessionFactory();
 		publisherRepository = new PublisherRepositoryHibernate(sessionFactory);
 	}
 
 	@AfterEach
 	void tearDown() {
 		sessionFactory.getSchemaManager().truncateMappedObjects();
+		sessionFactory.close();
 	}
 
 	@Test
 	void testFindAllWhenDatabaseIsEmpty() {
 		assertThat(publisherRepository.findAll()).isEmpty();
 	}
-	
+
 	@Test
-	void testFindAllWhenDatabaseIsNotEmpty(){
+	void testFindAllWhenDatabaseIsNotEmpty() {
 		addTestPublishers(new Publisher("test1"));
 		addTestPublishers(new Publisher("test2"));
-		assertThat(publisherRepository.findAll())
-			.containsExactly(
-					new Publisher(1,"test1"),
-					new Publisher(2,"test2")
-					);
+		assertThat(publisherRepository.findAll()).containsExactly(new Publisher(1, "test1"), new Publisher(2, "test2"));
 	}
-	
+
 	@Test
-	void testFindByIdWhenIsNotPresent(){
+	void testFindByIdWhenIsNotPresent() {
 		assertThat(publisherRepository.findById(1)).isNull();
 	}
-	
+
 	@Test
-	void testFindByIdWhenIsPresent(){
+	void testFindByIdWhenIsPresent() {
 		addTestPublishers(new Publisher("test1"));
 		addTestPublishers(new Publisher("test2"));
-		assertThat(publisherRepository.findById(2)).isEqualTo(new Publisher(2,"test2"));
+		assertThat(publisherRepository.findById(2)).isEqualTo(new Publisher(2, "test2"));
 	}
-	
+
 	@Test
-	void testSave(){
+	void testSave() {
 		Publisher toAdd = new Publisher("toAdd");
 		publisherRepository.save(toAdd);
-		assertThat(readAllPublisherFromDatabase())
-			.containsExactly(new Publisher(1,"toAdd"));
-			
+		assertThat(readAllPublisherFromDatabase()).containsExactly(new Publisher(1, "toAdd"));
 	}
-	
+
 	@Test
 	void testDelete() {
 		addTestPublishers(new Publisher("toDelete"));
@@ -95,15 +72,12 @@ class PublisherRepositoryHibernateTest {
 	}
 
 	private List<Publisher> readAllPublisherFromDatabase() {
-		return sessionFactory.fromTransaction(session ->
-		session.createSelectionQuery("from Publisher", Publisher.class)
-		.getResultList());
+		return sessionFactory.fromTransaction(
+				session -> session.createSelectionQuery("from Publisher", Publisher.class).getResultList());
 	}
-	
+
 	private void addTestPublishers(Publisher toAdd) {
-		sessionFactory.inTransaction(session ->
-			session.persist(toAdd));
+		sessionFactory.inTransaction(session -> session.persist(toAdd));
 	}
-	
 
 }
