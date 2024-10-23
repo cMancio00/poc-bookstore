3345aa0f0ee4debb8935d366955aa14a1e608366
Christian Mancini <mancinichristian00@gmail.com>
2024-10-23 12:18:17 +0200

    Removed test container and added log string

--- bookstore/src/test/java/mancio/bookstore/repository/PublisherRepositoryHibernateTest.java
+++ bookstore/src/test/java/mancio/bookstore/repository/PublisherRepositoryHibernateTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.AfterAll;
@@ -11,13 +13,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
 import mancio.bookstore.model.Publisher;
 
-@Testcontainers
 class PublisherRepositoryHibernateTest {
-	
+	private static final Logger LOGGER = LogManager.getLogger(PublisherRepositoryHibernateTest.class);
 	private static final String MYSQL_DATABASE = "bookstore";
 	private static final int MYSQL_PORT = 3306;
 	private static final  String CONNECTION_URL = String.format("jdbc:mysql://localhost:%d/%s", MYSQL_PORT, MYSQL_DATABASE);
@@ -31,6 +30,7 @@ class PublisherRepositoryHibernateTest {
 		System.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
 		System.setProperty("hibernate.connection.username", "bookstore-manager");
 		System.setProperty("hibernate.connection.password", "test");
+		LOGGER.info("Connecting to {}", CONNECTION_URL);
 		sessionFactory = new Configuration().configure().buildSessionFactory();
 	}
 
